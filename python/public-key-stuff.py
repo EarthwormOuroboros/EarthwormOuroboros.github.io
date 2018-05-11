70ba28896ae3ada46dec8b1266f2dd87ea793a0b
Lorenzo Wacondo <lorenzo.wacondo@staet.nm.us>
2018-05-10 18:32:18 -0600

    Fixed the 1st line.

--- python/public-key-stuff.py
+++ python/public-key-stuff.py
@@ -1,6 +1,6 @@
-/usr/bin/env python3
+#!/usr/bin/env python3
 
-import argpars
+import argparse
 from Crypto.Cipher import AES
 
 # https://www.blog.pythonlibrary.org/2016/05/18/python-3-an-intro-to-encryption/
@@ -14,21 +14,27 @@ parser.add_argument("-s","--set",action="store_true",
                     help="Set data.")
 
 # Encryption
-def encrypt-string(DATA, KEY, IV):
+def encrypt_string(DATA, KEY, IV):
     encryption_suite = AES.new(KEY, AES.MODE_CBC, IV)
     cipher_text = encryption_suite.encrypt(DATA)
     return cipher_text
 
 
 # Decryption
-def decrypt-string(DATA, KEY, IV):
+def decrypt_string(DATA, KEY, IV):
     decryption_suite = AES.new(KEY, AES.MODE_CBC, IV)
     plain_text = decryption_suite.decrypt(DATA)
     return plain_text
 
 
 def main():
-
-
+    iv = '1234567890abcdef'
+    payload = 'Whut3v4!'
+    
+    enc_data = encrypt_string(payload, key, iv)
+    enc_data
+    
+    dec_data = decrypt_string(enc_data, key, iv)
+    dec_data
 
 main()
