b13d9fd979941ce45eff96b998deff6b47ce69e2
lwacondo@gmail.com <lwacondo@gmail.com>
2018-05-11 02:03:41 -0600

    Simple start

--- python/public-key-stuff.py
+++ python/public-key-stuff.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from Crypto.Cipher import AES
-
-# https://www.blog.pythonlibrary.org/2016/05/18/python-3-an-intro-to-encryption/
-
-key = 'nooneknows'
+from cryptography.fernet import Fernet
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f","--fetch",action="store_true",
@@ -13,28 +9,29 @@ parser.add_argument("-f","--fetch",action="store_true",
 parser.add_argument("-s","--set",action="store_true",
                     help="Set data.")
 
-# Encryption
-def encrypt_string(DATA, KEY, IV):
-    encryption_suite = AES.new(KEY, AES.MODE_CBC, IV)
-    cipher_text = encryption_suite.encrypt(DATA)
-    return cipher_text
 
+def crypto_string(DATA, KEY, ACTION):
+    cipher = Fernet(KEY)
+    if ACTION == "encrypt":
+        # Encryption
+        data = cipher.encrypt(DATA)
+    elif ACTION == 'decrypt':
+        # Decryption
+        data = cipher.decrypt(DATA)
 
-# Decryption
-def decrypt_string(DATA, KEY, IV):
-    decryption_suite = AES.new(KEY, AES.MODE_CBC, IV)
-    plain_text = decryption_suite.decrypt(DATA)
-    return plain_text
+    return data
 
 
 def main():
-    iv = '1234567890abcdef'
-    payload = 'Whut3v4!'
-    
-    enc_data = encrypt_string(payload, key, iv)
-    enc_data
-    
-    dec_data = decrypt_string(enc_data, key, iv)
-    dec_data
+    cipher_key = Fernet.generate_key()
+    string = b'Whut3v4Mang!'
+
+    encrypted_text = crypto_string(string, cipher_key, 'encrypt')
+    print(encrypted_text)
+
+    decrypted_text = crypto_string(encrypted_text, cipher_key, 'decrypt')
+    print(decrypted_text)
+
 
 main()
+
