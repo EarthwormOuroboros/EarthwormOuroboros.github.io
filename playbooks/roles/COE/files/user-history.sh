c03c502aad994ed93d9a97ca1e05e2472d65d0c9
Lorenzo Wacondo <lwacondo@gmail.com>
2022-02-12 03:39:29 -0700

    Updated user history config.

---
+++ playbooks/roles/COE/files/user-history.sh
@@ -0,0 +1,7 @@
+#  /etc/profile.d/user-history.sh
+
+# Set bash history, file size and time format.
+HISTSIZE=1000
+HISTFILESIZE=40000
+HISTTIMEFORMAT="[%F %T %Z] "
+export HISTSIZE HISTFILESIZE HISTTIMEFORMAT
